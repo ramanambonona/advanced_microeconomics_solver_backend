2cf0d958535be49a079b0a5560fcab0131cebb1e
RAMANAMBONONA <105931833+ramanambonona@users.noreply.github.com>
2025-10-23 16:44:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -415,3 +415,8 @@ async def validation_error_handler(request, exc):
             "error": str(exc)
         }
     )
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
