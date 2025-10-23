4fff0729e14b8076efecdd35f5a559b28e021410
RAMANAMBONONA <105931833+ramanambonona@users.noreply.github.com>
2025-10-23 15:33:10 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+Advanced Microeconomics Solver Backend
+Solver symbolique pour l'optimisation microéconomique
+"""
+
+__version__ = "1.0.0"
+__author__ = "Université d'Antananarivo"
+__description__ = "Backend API pour le solveur microéconomique"
